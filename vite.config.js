e28b851a5ea4e003ef50622541fd63732305998f
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-05-12 14:42:29 +0530

    feat: add vite-plugin-html for enhanced HTML template management and update package.json dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'UOK Robot Battle 2K25 - Electronics And Computer Science Club',
+          description: 'UOK Robot Battle 2K25 is a dynamic robotics competition hosted by the Electronics and Computer Science Club of the University of Kelaniya. Open to all, it fosters creativity, technical skills, and innovation through exciting robotic challenges.',
+          keywords: 'robot battles, UOK, robotics competition, 2025, ECSC, ecsc',
+          author: 'ECSC CLUB - University of Kelaniya',
+        },
+      },
+    }),
+  ],
   server: {
     host: true, // <-- This is the magic
     port: 5174, // optional, just to fix the port if you want
   }
-})
+});
